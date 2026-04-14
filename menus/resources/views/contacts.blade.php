528a7eeeb3db27741a8e746c20bb19de24d99092
josemario234 <AL02963295@tecmilenio.mx>
2026-04-13 20:25:35 -0700

    Creating "contacts.blade.php" file

---
+++ menus/resources/views/contacts.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title', 'Contacts')
+@section('content')
+    <h1>Contacts</h1>
+    <p>This is the Contacts section</p>
+    <p>Where you can see all the contacts you have save.</p>
+@endsection
