414b201f9695c1086b83e0bc004d7463eccd8f29
josemario234 <AL02963295@tecmilenio.mx>
2026-04-13 20:59:47 -0700

    Modifying "web.php" file

--- menus/routes/web.php
+++ menus/routes/web.php
@@ -2,14 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    $menuSelection= [
-        "Home",
-        "Contacts",
-        "Reports"
-    ];
-    return view('welcome',
-    [
-        "menu" => $menuSelection
+$menu = [
+    'Home' => '/',
+    'Contacts' => '/contacts',
+    'Reports' => '/reports'
+];
+
+Route::get('/', function () use ($menu) {
+    return view('welcome', [
+        'menu' => $menu,
+        'active' => 'Home',
+    ]);
+});
+
+Route::get('/reports', function () use ($menu) {
+    return view('contacts', [
+        'menu' => $menu,
+        'active' => 'Contacts',
+    ]);
+});
+
+Route::get('/reports', function () use ($menu){
+    return view('reports', [
+        'menu' => $menu,
+        'active' => 'Reports',
     ]);
 });
