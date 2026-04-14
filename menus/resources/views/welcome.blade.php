27d049c652f28a55ea9e553ae7e69168bdd01184
josemario234 <AL02963295@tecmilenio.mx>
2026-04-13 20:05:36 -0700

    "welcome.blade.php"

--- menus/resources/views/welcome.blade.php
+++ menus/resources/views/welcome.blade.php
@@ -2,4 +2,6 @@
 @section("title", "Home")
 @section("content")
     <h1>Home</h1>
-    
+    <p>Welcome to the home section. This is the main menu</p>
+    <p>Here you can navigate to Contacts or Reports</p>
+@endsection
