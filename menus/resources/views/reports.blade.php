a66081184cc661b65b7623dab5019c84011dfb08
josemario234 <AL02963295@tecmilenio.mx>
2026-04-13 20:13:13 -0700

    Creating "reports.blade.php" file

---
+++ menus/resources/views/reports.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title', 'Reports')
+@section('content')
+    <h1>Reports</h1>
+    <p>This is the Report section</p>
+    <p>Where you can see the reports that you receive.</p>
+@endsection
